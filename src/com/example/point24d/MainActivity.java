175c71f9435a966101471b3ab3282a67ac10ef0a
zj <zj@webturing.com>
2019-01-07 16:08:33 +0800

    实现了软件退出的功能

--- src/com/example/point24d/MainActivity.java
+++ src/com/example/point24d/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.point24d;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnExit=(Button) super.findViewById(R.id.btnExit);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				System.exit(0);
+				
+			}
+		});
     }
 
 }
